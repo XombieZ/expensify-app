4a04e4a241f5de83ea597aa6377c77d641738c20
Alexandru <postolachealexandru@gmail.com>
2019-07-29 22:11:41 +0300

    add styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import numeral from "numeral";
 import getVisibleExpenses from "../selectors/expenses";
 import selectExpensesTotal from "../selectors/expenses-total";
 
 export const ExpensesSummary = ({ expenseCount, expenseTotal }) => {
   const sufix = expenseCount > 1 ? "s" : "";
-  const text = `Viewing ${expenseCount} expense${sufix} totalling $${numeral(
-    expenseTotal / 100
-  ).format("0.00")}`;
 
-  return <div>{expenseTotal > 0 && <p>{text}</p>}</div>;
+  return (
+    <div className="page-header">
+      <div className="content-container">
+        {expenseTotal > 0 && (
+          <h1 className="page-header__title">
+            Viewing <span>{expenseCount}</span> expense{sufix} totalling
+            <span> {numeral(expenseTotal / 100).format("$0,0.00")}</span>
+          </h1>
+        )}
+        <div className="page-header__actions">
+          <Link className="button" to="/create">
+            Add Expense
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
