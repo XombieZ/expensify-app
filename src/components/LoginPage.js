1fd3afe5243fd6c23540633ee88aed88893b869b
Alexandru <postolachealexandru@gmail.com>
2019-08-04 18:29:34 +0300

    added login buttons styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -10,15 +10,20 @@ export class LoginPage extends React.Component {
         <div className="box-layout__box">
           <h1 className="box-layout__title">Expensify</h1>
           <p>It's time to get your expenses under control. </p>
-          <button className="button" onClick={this.props.startLoginWithGoogle}>
-            Login with Google
-          </button>
-          <button
-            className="button"
-            onClick={this.props.startLoginWithFacebook}
-          >
-            Login with Facebook
-          </button>
+          <div className="box-layout__buttons">
+            <button
+              className="loginBtn loginBtn--google"
+              onClick={this.props.startLoginWithGoogle}
+            >
+              Login with Google
+            </button>
+            <button
+              className="loginBtn loginBtn--facebook"
+              onClick={this.props.startLoginWithFacebook}
+            >
+              Login with Facebook
+            </button>
+          </div>
         </div>
       </div>
     );
